d39f6a0ce3f30db5b02cf9418cbef35c21d78f7e
metasean <metasean@users.noreply.github.com>
2017-01-07 04:00:28 -0700

    test (dateTimePicker) Refactor eslint-env and vars

--- src/Components/DateTimePicker/spec.js
+++ src/Components/DateTimePicker/spec.js
@@ -1,32 +1,33 @@
-/* globals describe, it */
-var expect = require('expect')
-var createDateTime = require('./createDateTime').default
+/* eslint-env mocha */
+import expect from 'expect'
+import { shallow } from 'enzyme'
+import createDateTime from './createDateTime'
 
-describe('createDateTime', function () {
-	describe('given only a time', function () {
+describe('The createDateTime utility', function () {
+	context('given only a time', function () {
 		it('should return now with the updated time', function () {
-			var time = new Date('1970 01 01 4:20')
-			var newTime = createDateTime({time})
+			const time = new Date('1970 01 01 4:20')
+			const newTime = createDateTime({time})
 			expect(newTime.getHours()).toBe(4)
 			expect(newTime.getMinutes()).toBe(20)
 		})
 	})
 
-	describe('given only a date', function () {
+	context('given only a date', function () {
 		it('should return now with the updated date', function () {
-			var date = new Date('2020 12 31')
-			var newDate = createDateTime({date})
+			const date = new Date('2020 12 31')
+			const newDate = createDateTime({date})
 			expect(newDate.getFullYear()).toBe(2020)
 			expect(newDate.getMonth()).toBe(11) // 'cause JS month off by one
 			expect(newDate.getDate()).toBe(31)
 		})
 	})
 
-	describe('given a date and time', function () {
+	context('given a date and time', function () {
 		it('should return a new date time', function () {
-			var date = new Date('2020 12 31')
-			var time = new Date('1970 01 01 4:20')
-			var newDateTime = createDateTime({date, time})
+			const date = new Date('2020 12 31')
+			const time = new Date('1970 01 01 4:20')
+			const newDateTime = createDateTime({date, time})
 			expect(newDateTime.getFullYear()).toBe(2020)
 			expect(newDateTime.getMonth()).toBe(11) // 'cause JS month off by one
 			expect(newDateTime.getDate()).toBe(31)
@@ -35,4 +36,3 @@ describe('createDateTime', function () {
 		})
 	})
 })
-
