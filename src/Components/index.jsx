1842c49ef0ac76323b4bf83a59be5dafb2bd084a
Jesse Gibson <Jesse_Gibson@me.com>
2017-04-15 19:26:49 -0600

    feat(redux) Add redux logger

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 import UIProvider from 'material-ui/styles/MuiThemeProvider'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { render } from 'react-dom'
+import logger from 'redux-logger'
+import React from 'react'
 
 import reduxer from '../Reducers'
 
-const store = createStore(reduxer)
+const store = createStore(
+	reduxer,
+	applyMiddleware(logger)
+)
 
 /**
  * @see https://github.com/callemall/material-ui#react-tap-event-plugin
