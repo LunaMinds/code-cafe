c4bbbc6d92738b260ff55e02388f644f12ed0d68
Jesse Gibson <Jesse_Gibson@me.com>
2017-04-15 21:17:42 -0600

    chore(DateTimeRange) Hook components up to redux
    Start and end date times, including the defaults, are plugged into redux
    and synchronized with the UI.

--- src/Components/DateTimeRange/spec.js
+++ src/Components/DateTimeRange/spec.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 import React from 'react'
-import expect from 'expect'
+import expect, { createSpy } from 'expect'
 import { shallow } from 'enzyme'
 
 import { DateTimeRange } from './index'
@@ -11,31 +11,79 @@ describe('<DateTimeRange>', () => {
 
 	beforeEach(() => {
 		props = {
-			setEventTimes: expect.createSpy(),
+			setEventTimes: createSpy(),
+			start: 123,
+			end: 456,
 		}
 
 		range = setup()
 	})
 
-	it('has a start time by default', () => {
-		const picker = range.find('DateTimePicker').at(0)
-		const time = picker.prop('defaultDateTime')
-
-		expect(time).toBeA(Date)
-	})
-
-	it('has an end time by default', () => {
-		const picker = range.find('DateTimePicker').at(1)
-		const time = picker.prop('defaultDateTime')
-
-		expect(time).toBeA(Date)
-	})
-
 	it('defaults to an hour duration', () => {
-		const start = range.find('DateTimePicker').at(0).prop('defaultDateTime')
-		const end = range.find('DateTimePicker').at(1).prop('defaultDateTime')
+		range.instance().componentWillMount()
+
+		expect(props.setEventTimes).toHaveBeenCalled()
+
+		const {start, end} = props.setEventTimes.calls[0].arguments[0]
 		const hour = 1000 * 60 * 60
 
-		expect(end.getTime() - start.getTime()).toBe(hour)
+		expect(end - start).toBe(hour)
+	})
+
+	it('updates when the start time is changed', () => {
+		const date = new Date(123)
+		range.find('DateTimePicker').at(0).simulate('change', date)
+
+		expect(props.setEventTimes).toHaveBeenCalled()
+		expect(props.setEventTimes).toHaveBeenCalledWith({
+			start: date.getTime(),
+		})
+	})
+
+	it('updates when the end time is changed', () => {
+		const date = new Date(456)
+		range.find('DateTimePicker').at(1).simulate('change', date)
+
+		expect(props.setEventTimes).toHaveBeenCalled()
+		expect(props.setEventTimes).toHaveBeenCalledWith({
+			end: date.getTime(),
+		})
+	})
+
+	it('provides a default start and end date on mount', () => {
+		range.instance().componentWillMount()
+
+		expect(props.setEventTimes).toHaveBeenCalled()
+
+		const {start, end} = props.setEventTimes.calls[0].arguments[0]
+
+		expect(start).toBeA('number')
+		expect(end).toBeA('number')
+	})
+
+	it('uses the correct start date time', () => {
+		const dateTime = range.find('DateTimePicker').at(0).prop('value')
+
+		expect(dateTime).toBeA(Date)
+		expect(dateTime.getTime()).toBe(props.start)
+	})
+
+	it('uses the correct end date time', () => {
+		const dateTime = range.find('DateTimePicker').at(1).prop('value')
+
+		expect(dateTime).toBeA(Date)
+		expect(dateTime.getTime()).toBe(props.end)
+	})
+
+	it('provides default date and time values', () => {
+		props.start = undefined
+		props.end = undefined
+		range = setup()
+
+		const startDateTime = range.find('DateTimePicker').at(0).prop('value')
+		const endDateTime = range.find('DateTimePicker').at(1).prop('value')
+
+		expect(isNaN(startDateTime.getTime())).toBe(false)
+		expect(isNaN(endDateTime.getTime())).toBe(false)
 	})
 })
